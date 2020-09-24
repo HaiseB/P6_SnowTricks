2013509312b59db2620961d1dae9b62de69505af
Benjamin HAISE <43814190+HaiseB@users.noreply.github.com>
2020-09-24 11:11:24 +0200

    before webpack

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,17 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Trick;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}", methods="POST")
+     * @Route("/commentsByTrick/{trick}", name="app_comment_show", methods="POST")
      */
-    public function comment() {
+    public function trickComments(Trick $trick, CommentRepository $commentRepository) {
+        $comments = $commentRepository->findBy(
+            ['trick' => $trick]
+        );
 
-        return $this->json(['comments' => 92]);
+        dd($comments);
+
+        return $this->json([$comments]);
+    }
+
+    /**
+     * @Route("/comment/new/{trick}", name="app_comment_new")
+     */
+    public function new(EntityManagerInterface $em, Request $request, Trick $trick)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $form = $this->createForm(CommentFormType::class );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Comment $comment */
+            $comment = $form->getData();
+            $comment->setUser($this->getUser());
+            $comment->setTrick($trick);
+
+            $em->persist($comment);
+            $em->flush();
+
+            return $this->redirectToRoute('app_trick_show', ['id' => $trick->getId()]);
+        }
+
+        return $this->render(
+            'comment/create.html.twig', [
+                'commentForm' => $form->createView()
+            ]
+        );
     }
 }
